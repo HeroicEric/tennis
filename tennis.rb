b49489a91570164a195e8f19c68b273d1a496fcc
Eric Kelly <heroiceric@gmail.com>
2011-06-18 17:33:22 -0400

    Fixed connection to Database

--- tennis.rb
+++ tennis.rb
@@ -5,7 +5,7 @@ Bundler.require
 require 'net/http'
 require 'uri'
 
-enable :run
+enable :run, true
 set :root, File.join(File.dirname(__FILE__), "..")
 set :haml, { :format => :html5 } # default for Haml format is :xhtml
 
